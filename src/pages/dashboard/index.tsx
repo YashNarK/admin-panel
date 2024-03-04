13c15aca1c35890416c45bffde2bfe6dd1f5947a
YashNarK <narenkrithick@gmail.com>
2024-03-04 17:34:47 +0530

    responsive line chart with old and new data added

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,7 +6,18 @@ import { ResponsiveAreaChart } from "../../components/dashboard/ResponsiveAreaCh
 import { ResponsiveBarChart } from "../../components/dashboard/ResponsiveBarChart";
 import { TabView } from "../../components/dashboard/TabView";
 import { RecentSales } from "../../components/dashboard/RecentSales";
-import { IChartDatum, TTab } from "../../interfaces";
+import { IChartDatum, ILineData, TTab } from "../../interfaces";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { revenueDataset_250223_030323 } from "../../data/data";
 
 const filters: CrudFilter[] = [
   {
@@ -21,7 +32,13 @@ const filters: CrudFilter[] = [
   },
 ];
 
+const pickDateRange = () => {
+  let value1 = dayjs()?.subtract(7, "days")?.startOf("day");
+  let value2 = dayjs().startOf("day");
+};
+
 export const Dashboard: React.FC = () => {
+  // hooks
   const { data: dailyRevenue } = useList<IChartDatum>({
     resource: "dailyRevenue",
     filters,
@@ -51,24 +68,78 @@ export const Dashboard: React.FC = () => {
   };
 
   const memoizedRevenueData = useMemoizedChartData(dailyRevenue);
+  const memoizedRevenueDataOld = useMemoizedChartData(
+    revenueDataset_250223_030323
+  );
+
   const memoizedOrdersData = useMemoizedChartData(dailyOrders);
   const memoizedNewCustomersData = useMemoizedChartData(newCustomers);
 
+  // helper functions
+  function extractDateWithoutYear(dateString: string) {
+    return dayjs(dateString).format("DD, MMM");
+  }
+  function prepareLineData() {
+    let i = 0;
+
+    return memoizedRevenueData?.map((revenueData: IChartDatum) => {
+      let newData = {
+        ...revenueData,
+        oldValue: memoizedRevenueDataOld[i].value,
+        date: extractDateWithoutYear(revenueData.date),
+      };
+      i += 1;
+      return newData;
+    });
+  }
+  const lineData: ILineData[] = prepareLineData();
+
+  // debugs
+  // console.log("new", dailyRevenue);
+  // console.log("old", revenueDataset_250223_030323);
+  console.log("new", memoizedRevenueData);
+  console.log("old", memoizedRevenueDataOld);
+  console.log(lineData);
+
+  // components
   const tabs: TTab[] = [
     {
       id: 1,
       label: "Daily Revenue",
       content: (
-        <ResponsiveAreaChart
-          kpi="Daily revenue"
-          data={memoizedRevenueData}
-          colors={{
-            stroke: "rgb(54, 162, 235)",
-            fill: "rgba(54, 162, 235, 0.2)",
-          }}
-        />
+        <ResponsiveContainer
+          width={"100%"}
+          height={400}
+          id="chart-line"
+          className="rounded-md mx-auto"
+        >
+          <LineChart data={lineData}>
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+            <Line
+              type="monotone"
+              dataKey="value"
+              stroke="#8884d8"
+              strokeWidth={3}
+              name="2024"
+            />
+
+            <Line
+              type="monotone"
+              dataKey="oldValue"
+              stroke="#ddedf8"
+              strokeWidth={3}
+              strokeDasharray={"9 3"}
+              name="2023"
+            />
+          </LineChart>
+        </ResponsiveContainer>
       ),
     },
+
     {
       id: 2,
       label: "Daily Orders",
