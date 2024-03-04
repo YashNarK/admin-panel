eff54320d51145a20b7c346da9c448d178b20158
YashNarK <narenkrithick@gmail.com>
2024-03-04 22:46:29 +0530

    Custom tool tip component created

---
+++ src/components/CustomTooltip/CustomTooltip.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { HiArrowTrendingDown, HiArrowTrendingUp } from "react-icons/hi2";
+import { TooltipProps } from "recharts";
+import styles from "./dashboard.module.css";
+type CustomTooltipProps = TooltipProps<number, string>;
+
+const CustomTooltip: React.FC<CustomTooltipProps> = ({
+  active,
+  payload,
+  label,
+}) => {
+  if (active && payload && payload.length) {
+    console.log(payload);
+    let newVal = payload[0].payload.value;
+    let oldVal = payload[1].payload.oldValue;
+    let diff =
+      ((Math.max(newVal, oldVal) - Math.min(newVal, oldVal)) /
+        Math.max(newVal, oldVal)) *
+      100;
+
+    return (
+      <>
+        <div className="stats shadow">
+          <div className="stat">
+            <div
+              className={[
+                "stat-figure",
+                `text-${newVal > oldVal ? "success" : "error"}`,
+              ].join(" ")}
+            >
+              {newVal > oldVal ? (
+                <HiArrowTrendingUp />
+              ) : (
+                <HiArrowTrendingDown />
+              )}{" "}
+              {diff.toFixed(2)}%
+            </div>
+            <div className="new-value flex">
+              <div className="badge my-auto mr-2">
+                <div className={["h-1 w-4", styles.bg8884d8].join(" ")}></div>
+              </div>
+              <div className="my-auto">
+                <p>{`${label} ${payload[0].name} ${newVal}`}</p>
+              </div>
+            </div>
+            <div className="old-value flex">
+              <div className="badge my-auto mr-2">
+                <div className={["h-1 w-4", styles.bgddedf8].join(" ")}></div>
+              </div>
+              <div className="my-auto">
+                <p>{`${label} ${payload[1].name} ${oldVal}`}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return null;
+};
+
+export default CustomTooltip;
